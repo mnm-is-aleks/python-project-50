941b44e9c08a2c5641b9b24a2e2f89b24e44c984
user <user@Python.myguest.virtualbox.org>
2024-01-13 22:27:14 +0100

    add gendiff.py

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+import argparse
+
+
+parser = (
+    argparse.ArgumentParser
+    (description='Compares two configuration files and shows a difference.'))
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+parser.print_help()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
