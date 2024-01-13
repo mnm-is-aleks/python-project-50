362d1feb59c35bfecdecbb00ab4b128e78144849
user <user@Python.myguest.virtualbox.org>
2024-01-13 23:00:59 +0100

    add arg --format to gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,8 @@ import argparse
 parser = (
     argparse.ArgumentParser
     (description='Compares two configuration files and shows a difference.'))
+parser.add_argument('-f', '--format', type=str, help='set format of output')
+
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.print_help()
